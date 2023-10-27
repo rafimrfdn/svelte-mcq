74b7f0c9089d217a29f13b94486e2fa55d07aafb
rafimrfdn <uphy.mks@gmail.com>
2023-10-27 20:48:52 +0800

    update, hapus node:fs dari +server.js

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -1,9 +1,11 @@
 import { error, json } from '@sveltejs/kit';
 
 import Database from "better-sqlite3";
-import fs from "node:fs";
+//import fs from "node:fs";
 
-let db = new Database(fs.readFileSync("banksoal.db"));
+//let db = new Database(fs.readFileSync("banksoal.db"));
+let db = new Database("banksoal.db");
+
 // let db = new Database(fs.readFileSync("./src/database/banksoal.db"));
 
 export function GET() {
