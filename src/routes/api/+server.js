3db10f17784d323b18c040766f87d74ff64ab4fe
rafimrfdn <uphy.mks@gmail.com>
2023-10-27 20:37:37 +0800

    update

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -4,6 +4,7 @@ import Database from "better-sqlite3";
 import fs from "node:fs";
 
 let db = new Database(fs.readFileSync("banksoal.db"));
+// let db = new Database(fs.readFileSync("./src/database/banksoal.db"));
 
 export function GET() {
   const items = db.prepare("select * from banksoal");
