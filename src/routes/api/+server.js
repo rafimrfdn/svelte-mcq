efa8eb773b881ed614f4e6e57b589defc24f9e64
rafimrfdn <uphy.mks@gmail.com>
2023-11-16 08:32:40 +0800

    update menambahkan hint

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -1,14 +1,17 @@
+import { DB_PATH } from '$env/static/private';
 import { error, json } from '@sveltejs/kit';
 
 import Database from "better-sqlite3";
-//import fs from "node:fs";
+import fs from "node:fs";
 
 //let db = new Database(fs.readFileSync("banksoal.db"));
-let db = new Database("banksoal.db");
+//let db = new Database("banksoal.db");
+//let db = new Database(DB_PATH, { verbose: console.log });
+let db = new Database(fs.readFileSync(DB_PATH, { verbose: console.log }));
 
 // let db = new Database(fs.readFileSync("./src/database/banksoal.db"));
 
 export function GET() {
-  const items = db.prepare("select * from banksoal");
+  const items = db.prepare("select * from soaltwk");
   return json(items.all());
 }
