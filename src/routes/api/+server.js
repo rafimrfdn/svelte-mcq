57ee3366a9699bede9975f3b1e9bb3aa0dd88a2d
rafimrfdn <uphy.mks@gmail.com>
2023-11-17 20:53:18 +0800

    add prerender true

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -4,6 +4,8 @@ import { error, json } from '@sveltejs/kit';
 import Database from "better-sqlite3";
 import fs from "node:fs";
 
+export const prerender = true;
+
 //let db = new Database(fs.readFileSync("banksoal.db"));
 //let db = new Database("banksoal.db");
 //let db = new Database(DB_PATH, { verbose: console.log });
