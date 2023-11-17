c815286c2ac1b58a9125adbc922e0eeeff2afefa
rafimrfdn <uphy.mks@gmail.com>
2023-11-17 20:20:09 +0800

    reset my setting

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -1,4 +1,4 @@
-import { DB_PATH } from '$env/static/private';
+import { VITE_DB_PATH } from '$env/static/private';
 import { error, json } from '@sveltejs/kit';
 
 import Database from "better-sqlite3";
@@ -7,7 +7,7 @@ import fs from "node:fs";
 //let db = new Database(fs.readFileSync("banksoal.db"));
 //let db = new Database("banksoal.db");
 //let db = new Database(DB_PATH, { verbose: console.log });
-let db = new Database(fs.readFileSync(DB_PATH, { verbose: console.log }));
+let db = new Database(fs.readFileSync(VITE_DB_PATH, { verbose: console.log }));
 
 // let db = new Database(fs.readFileSync("./src/database/banksoal.db"));
 
